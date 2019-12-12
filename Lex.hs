e8f86dc7e431c1c3e7576b99e1be16628be13e35
Calle Edlund <calle.edlund21@gmail.com>
2019-12-12 17:01:08 +0100

    .

--- Lex.hs
+++ Lex.hs
@@ -8,26 +8,29 @@ import Ubi
 import Util
 
 lex' :: Sweep
-lex' path (Undone s) = Lexed $ loop 1 1 s
+lex' path (Input s) = Lexed $ loop 1 1 s
   where
   loop :: Int -> Int -> C.ByteString -> [Tok2]
   loop col line inp
     | C.null inp = []
-    | otherwise  = if col == 1 && synMultiline inp then multiLine (line + 1) dropLine' inp
+    | otherwise  = if col == 1 && synMultiline inp then multiLine (line + 1) $ fromJust $ dropLine inp
                    else case C.head inp of
                           ' '  -> let (n, inp') = countAndDropUntil (/=' ') inp
                                   in loop (col + n) line inp'
                           '\n' -> let (n, inp') = countAndDropUntil (/='\n') inp
                                   in loop 1 (line + n) inp'
-                          c    -> if C.pack "\\\\" `C.isPrefixOf` inp then loop col line $ C.dropWhile (/='\n') inp
+                          c    -> if C.pack "\\\\" `C.isPrefixOf` inp
+                                  then case dropLine inp of
+                                         Nothing   -> []
+                                         Just inp' -> loop 1 (line + 1) inp'
                                   else let (tok, len, inp') = token c
                                        in (col, line, tok) : loop (col + len) line inp'
     where
-    multiLine :: C.ByteString -> [Tok2]
-    multiLine line inp | synMultiline inp = loop 1 (line + 1) dropLine' inp
+    multiLine :: Int -> C.ByteString -> [Tok2]
+    multiLine line inp | synMultiline inp = loop 1 (line + 1) $ dropLine' inp
                        | otherwise        = let inp' = dropLine inp
                                             in case inp' of
-                                                 Nothing    -> lError (-1) line "File ended in multiline comment"
+                                                 Nothing    -> lError 1 line "File ended in multiline comment"
                                                  Just inp'' -> multiLine (line + 1) inp''
     token :: Char -> (Tok, Int, C.ByteString)
     token c = if punctuation c then (Punctuation c, 1, C.tail inp)
@@ -39,15 +42,15 @@ lex' path (Undone s) = Lexed $ loop 1 1 s
                                        in (TokString s', i + 2, C.drop (i + 1) start)
                              _      -> lError col line path "Couldn't find end of string"
                 '\'' -> let (s', inp') = C.span (symChar ||| punctuation) (C.tail inp)
-                            c | C.length s' == 1   = C.head s'
-                              | charword s'        = charwordToChar s'
-                              | C.null s'          = if C.null inp'
-                                                     then lError (col + 1) line path "Expected something"
-                                                     else if C.head inp' == ' '
-                                                          then ' '
-                                                          else lError (col + 1) line path "Bad char"
-                              | synHexChar s'      = toEnum $ readint 16 s'
-                              | otherwise          = lError col line path "Bad char"
+                            c | C.length s' == 1 = C.head s'
+                              | charword s'      = charwordToChar s'
+                              | C.null s'        = if C.null inp'
+                                                   then lError (col + 1) line path "File ended after char character (')"
+                                                   else if C.head inp' == ' '
+                                                        then ' '
+                                                        else lError (col + 1) line path "Expected a space"
+                              | synHexChar s'    = toEnum $ readint 16 s'
+                              | otherwise        = lError col line path "Bad char"
                         in (TokChar c, 1 + C.length s', inp')
                 _    -> let (s', inp')             = C.span symChar inp
                             sym | C.null s'        = lError col line path ("Illegal character : " ++ [c])
